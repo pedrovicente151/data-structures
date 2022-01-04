edd8915549911b6082895e5981cd759ab72082e1
Pedro Vicente <pe.vicentee@gmail.com>
2022-01-03 23:07:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Test
+import sys
+
+data = []
+n = 26
+for i in range(n):
+    a = len(data)
+    b = sys.getsizeof(data)
+    print('Length: {0:3d}; Size in bytes: {1:4}'.format(a, b))
+    data.append(None)  # increase len by one
