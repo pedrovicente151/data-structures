289946a9af1ed07df30607548b51a9bd4638376e
Pedro Vicente <pe.vicentee@gmail.com>
2022-01-09 23:59:16 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from time import time
 
 data = []
 n = 26
@@ -7,3 +8,12 @@ for i in range(n):
     b = sys.getsizeof(data)
     print('Length: {0:3d}; Size in bytes: {1:4}'.format(a, b))
     data.append(None)  # increase len by one
+
+
+def compute_average(n):
+    data = []
+    start = time()
+    for i in range(n):
+        data.append(None)
+    end = time()
+    return (end - start) / n
