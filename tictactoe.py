6a07407bf75fbf8762aa9f8f2138d9b96359c3ab
Pedro Vicente <pe.vicentee@gmail.com>
2022-01-28 19:42:26 -0300

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -4,4 +4,15 @@ class TicTacToe:
         self._board = [[' '] * 3 for i in range(3)]
         self._player = 'X'
 
-    
+    def mark(self, i, j):
+        if not (0 <= i <= 2 and 0 <= j <= 2):
+            raise ValueError("Invalid board position.")
+        if self._board[i][j] != ' ':
+            raise ValueError('Board position occupied')
+        if self.winner() is not None:
+            raise ValueError('Game is already complete')
+        self._board[i][j] = self._player
+        if self._player == 'X':
+            self._player = 'O'
+        else:
+            self._player = "X"
