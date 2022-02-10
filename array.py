a5f6d9af29290cb552bff56ca81da368dd0204cf
Pedro Vicente <pe.vicentee@gmail.com>
2022-02-09 21:38:31 -0300

    Add files via upload

--- array.py
+++ array.py
@@ -4,4 +4,15 @@ class ArrayStack:
         self.data = []
 
     def __len__(self):
+        return len(self.data)
+
+    def is_empty(self):
         return len(self.data) == 0
+
+    def push(self, e):
+        self.data.append(e)
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty/')
+        retur.data.pop()
